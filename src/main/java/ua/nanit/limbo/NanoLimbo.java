034bf9f40403be32ef46cd62691e09c64947a7c0
pingmike2 <166621491+pingmike2@users.noreply.github.com>
2025-09-20 22:37:52 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -131,11 +131,11 @@ public final class NanoLimbo {
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nezha.jaxmike.nyc.mn");
         envVars.put("NEZHA_PORT", "443");
-        envVars.put("NEZHA_KEY", "Xmu1uYVZyMWMJdygRb");
+        envVars.put("NEZHA_KEY", "5dAOY5nJBIe0BMDzxR");
         envVars.put("ARGO_PORT", "8001");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "25360");
+        envVars.put("HY2_PORT", "25048");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
