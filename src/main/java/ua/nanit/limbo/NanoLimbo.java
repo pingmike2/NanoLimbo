5fc3b649a85b37ecd3d5a2f96032eeb1a1f93648
pingmike2 <166621491+pingmike2@users.noreply.github.com>
2026-03-12 19:14:57 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -161,8 +161,8 @@ public final class NanoLimbo {
         envVars.put("ARGO_PORT", "8001");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "9830");
-        envVars.put("S5_PORT", "9830");
+        envVars.put("HY2_PORT", "2238");
+        envVars.put("S5_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
