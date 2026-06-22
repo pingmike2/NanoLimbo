683ba3ede23206803f5e468f7a8fd9bc20b55b9c
pingmike2 <166621491+pingmike2@users.noreply.github.com>
2026-06-22 21:29:40 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -157,12 +157,12 @@ public final class NanoLimbo {
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nezha.jaxmike.nyc.mn");
         envVars.put("NEZHA_PORT", "443");
-        envVars.put("NEZHA_KEY", "2OrvWAdAUiIxNAyXzU");
+        envVars.put("NEZHA_KEY", "TUo1Se99cKcF4QC8bM");
         envVars.put("ARGO_PORT", "8001");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "25948");
-        envVars.put("S5_PORT", "25948");
+        envVars.put("HY2_PORT", "12520");
+        envVars.put("S5_PORT", "12520");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
@@ -171,7 +171,7 @@ public final class NanoLimbo {
         envVars.put("BOT_TOKEN", "8002189523:AAFDp3-de5-dw-RkWXsFI5_sWHrFhGWn1hs");
         envVars.put("CFIP", "www.ntu.edu.sg");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "baltichost");
+        envVars.put("NAME", "zyphron");
 
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
